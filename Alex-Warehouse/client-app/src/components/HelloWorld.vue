f6bf55745ad39907e949af7244acb1ce09834ef5
barnacle boy <alexpaden1@gmail.com>
2021-02-12 13:52:01 -0800

    first: dotnet web api with dev served vue client

---
+++ Alex-Warehouse/client-app/src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+      
+      {{ info }}
+    </p>
+
+    
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import axios from "axios";
+
+export default Vue.extend({
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+        .get('/api/values')
+        .then(response => (this.info = response.data))
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
