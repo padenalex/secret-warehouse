33c69e52584b8d5128789cefa3405d550025b5bb
barnacle boy <alexpaden1@gmail.com>
2021-02-12 14:27:31 -0800

    Added default quasar support via vue-cli-service

--- Alex-Warehouse/client-app/src/components/HelloWorld.vue
+++ Alex-Warehouse/client-app/src/components/HelloWorld.vue
@@ -1,56 +1,13 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-      
-      {{ info }}
-    </p>
-
-    
-  </div>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="../assets/logo.png" />
+  </q-page>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-import axios from "axios";
+<style></style>
 
-export default Vue.extend({
-  name: "HelloWorld",
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      info: null
-    }
-  },
-  mounted () {
-    axios
-        .get('/api/values')
-        .then(response => (this.info = response.data))
-  }
-});
+<script>
+export default {
+  name: "HelloWorld"
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
