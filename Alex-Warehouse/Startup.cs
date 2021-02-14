98f06b10bb6490abcc905b63e0d1744afec4bd58
barnacle boy <alexpaden1@gmail.com>
2021-02-14 11:40:09 -0800

    Docker file for deploying to local. Checking google post git

--- Alex-Warehouse/Startup.cs
+++ Alex-Warehouse/Startup.cs
@@ -44,16 +44,21 @@ namespace Secret_Warehouse
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Secret_Warehouse v1"));
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
-
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+            
             app.UseRouting();
-
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
             
-            app.UseSpaStaticFiles();
+
             app.UseSpa(spa =>
             {
                 spa.Options.SourcePath = "client-app";
