437f03a3b5cebcd09ed2fba7bf394417b6d34d70
barnacle boy <alexpaden1@gmail.com>
2021-02-18 12:41:53 -0800

    Added Insecure (public credential store) VPC connection for private IP postgres cloud sql instance. Functioning

---
+++ Alex-Warehouse/Models/Thing.cs
@@ -0,0 +1,9 @@
+namespace Secret_Warehouse.Models
+{
+    public class Thing
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
