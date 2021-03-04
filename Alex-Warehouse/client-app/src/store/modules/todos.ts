599286792b41ba6ccadd74941522f08e9e7f2db9
barnacle boy <alexpaden1@gmail.com>
2021-03-04 11:07:08 -0800

    Basic todo tasks page w/ vuex module. Rough draft

---
+++ Alex-Warehouse/client-app/src/store/modules/todos.ts
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+const state = {
+    todos: []
+};
+
+const getters = {
+    allTodos: (state: { todos: Array<any>; }) => state.todos
+};
+
+const actions = {
+    async fetchTodos({ commit }: {commit: Function}) {
+        const response = await axios.get(
+            'https://jsonplaceholder.typicode.com/todos'
+        );
+
+        commit('setTodos', response.data);
+    },
+    
+    async updateTodo({ commit }: {commit: Function}, updTodo: { id: any; }) {
+        const response = await axios.put(
+            `https://jsonplaceholder.typicode.com/todos/${updTodo.id}`,
+            updTodo
+        );
+
+        console.log(response.data);
+
+        commit('updateTodo', response.data);
+    }
+};
+
+const mutations = {
+    setTodos: (state: { todos: Array<any>; }, todos: Array<any>) => (state.todos = todos),
+    updateTodo: (state: { todos: Array<any>; }, updTodo: any) => {
+        const index = state.todos.findIndex(todo => todo.id === updTodo.id);
+        if (index !== -1) {
+            state.todos.splice(index, 1, updTodo);
+        }
+    }
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
---
+++ Alex-Warehouse/client-app/src/views/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="tasks">
+      <h1>This is an task page</h1>
+      <Todos/>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import Todos from "@/components/Todos";
+
+export default {
+  name: 'Tasks',
+  components: {
+    Todos,
+  }
+}
+</script>
