32ce27edb1e4f70abeb68ba61f31181ec36422dd
barnacle boy <alexpaden1@gmail.com>
2021-03-04 11:07:08 -0800

    Basic todo tasks page w/ vuex module. Rough draft

--- Alex-Warehouse/client-app/src/router/index.ts
+++ Alex-Warehouse/client-app/src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Resume from "../views/Resume.vue";
 import About from "../views/About.vue";
+import Tasks from "../views/Tasks.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes: Array<RouteConfig> = [
     path: "/about",
     name: "About",
     component: About
+  },
+  {
+    path: "/tasks",
+    name: "Tasks",
+    component: Tasks
   }
 ];
 
